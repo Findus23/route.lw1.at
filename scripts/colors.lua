906afcaabb25205c43c342c4236711f1d3565702
Lukas Winkler <git@lw1.at>
2026-01-02 20:08:06 +0100

    fix lua color script

--- scripts/colors.lua
+++ scripts/colors.lua
@@ -3,9 +3,7 @@
 -- SPDX-License-Identifier: AGPL-3.0-or-later
 
 
-local color_data = require 'scripts.color-data'
-local colors_by_agency_and_name = color_data.colors_by_agency_and_name
-local colors_by_id = color_data.colors_by_id
+local colors_by_id = require 'scripts.color-data'
 
 
 function process_route(route)
@@ -16,11 +14,6 @@ function process_route(route)
     local original_route_text_color = route:get_text_color()
     if (original_route_color ~= 0 or original_route_text_color ~= 0) then
        return
-    end
-    if colors_by_agency_and_name[agency_name] and colors_by_agency_and_name[agency_name][route_name] then
-        local color = colors_by_agency_and_name[agency_name][route_name]
-        route:set_color(color.color)
-        route:set_text_color(color.text_color)
     end
     if colors_by_id[route_id] then
         print("found")
