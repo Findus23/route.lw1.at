44779328ee47bc29c8304450abd8e4b99ab236f3
Lukas Winkler <git@lw1.at>
2026-01-10 01:26:25 +0100

    Revert "make all VOR busses green unless something more specific overrides this"
    This reverts commit 7d3c445fce9f3811f328722caac02b373c528a0c.

--- color-data/query.sql
+++ color-data/query.sql
@@ -10,7 +10,7 @@ where dataset == "vor"
   and colour not null;
 
 
-SELECT o.name,
+EXPSELECT o.name,
        o.ref,
        o.colour,
        o.colour_text,
@@ -22,18 +22,3 @@ FROM osm_routes AS o
                   AND (pt.ref IS NULL OR o.ref = pt.ref)
                   AND (pt."from" IS NULL OR o."from" = pt."from")
 WHERE o.colour IS NOT NULL and pt.gtfs_route_id is not null;
-
-
-SELECT o.name,
-       o.ref,
-       o.colour,
-       o.colour_text,
-       pt.gtfs_route_id,
-       o.rel_id,gtfs_routes.route_type
-FROM osm_routes AS o
-         JOIN main.ptna_table AS pt
-              ON (pt.operator IS NULL OR o.operator = pt.operator)
-                  AND (pt.ref IS NULL OR o.ref = pt.ref)
-                  AND (pt."from" IS NULL OR o."from" = pt."from")
-        join gtfs_routes on gtfs_routes.route_id == pt.gtfs_route_id
-WHERE o.colour IS NULL and gtfs_routes.route_type!=3;
