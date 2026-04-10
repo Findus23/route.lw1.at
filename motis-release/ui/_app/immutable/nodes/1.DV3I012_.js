00b1b889bc4192ec170d98877af0301b753bb94c
Lukas Winkler <git@lw1.at>
2026-04-11 00:57:20 +0200

    update ui to patched 2.9.2

--- motis-release/ui/_app/immutable/nodes/1.DV3I012_.js
+++
@@ -1,2 +0,0 @@
-import{E as m}from"../chunks/svCfQ5nm.js";export{m as component};
-//# sourceMappingURL=1.DV3I012_.js.map
---
+++ motis-release/ui/_app/immutable/nodes/1.hh4L_BOs.js
@@ -0,0 +1,2 @@
+import{E as m}from"../chunks/CXwFGouV.js";export{m as component};
+//# sourceMappingURL=1.hh4L_BOs.js.map
